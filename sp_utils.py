1e9c1531218eadecea3522bbb6add451e0f7ad44
Evan Jones <evan_t_jones@mac.com>
2012-01-23 10:42:48 -0800

    sp_utils.py:  added france, added leading underscore to signify some private methods

--- sp_utils.py
+++ sp_utils.py
@@ -11,7 +11,7 @@ TAU = 2*pi
 
 UP_VEC      = [ 0, 0, 1]
 RIGHT_VEC   = [ 1, 0, 0]
-BACK_VEC    = [ 0, 1, 0]
+FORWARD_VEC = [ 0, 1, 0]
 
 # ==========
 # = Colors =
@@ -117,20 +117,20 @@ def arc( rad, start_degrees, end_degrees, invert=False, segments=None):
     # second point is on the square, but in line (vertically or horizontally) with start_point
     # penultimate_point is the same w.r.t to end_point
     if start_degrees not in [0, 90, 180, 270, 360]: 
-        points.append( side_point_for_degrees( start_degrees, rad))
+        points.append( _side_point_for_degrees( start_degrees, rad))
     
     # From start_point, work counterclockwise to end_point, with all intermediate 
     # points on the circumscribed square
-    start_side = which_side(start_degrees)
-    end_side = which_side( end_degrees)
+    start_side = _which_side(start_degrees)
+    end_side = _which_side( end_degrees)
     if end_side < start_side:
         end_side += 4
     for side in range( start_side, end_side):
-        points.append( next_corner_for_side( side, rad))
+        points.append( _next_corner_for_side( side, rad))
         
     
     if end_degrees not in [0, 90, 180, 270, 360]: 
-        points.append( side_point_for_degrees( end_degrees, rad))
+        points.append( _side_point_for_degrees( end_degrees, rad))
     points.append( end_point)
     paths = [range(len(points))]
     
@@ -152,7 +152,7 @@ def arc( rad, start_degrees, end_degrees, invert=False, segments=None):
 # =========================
 # = Arc Helpers... ignore =
 # =========================
-def next_corner_for_side( side, rad):
+def _next_corner_for_side( side, rad):
     corners = { RIGHT:  [rad, rad],
                 TOP:    [-rad, rad],
                 LEFT:   [-rad, -rad],
@@ -160,14 +160,14 @@ def next_corner_for_side( side, rad):
          }
     return corners[side % 4]
 
-def side_point_for_degrees( degrees, rad):
+def _side_point_for_degrees( degrees, rad):
     # returns a line on a circle's circumscribed square
     # that is horizontally or vertically aligned with the
     # point at degrees on the circle
     x = rad * cos( radians( degrees))
     y = rad * sin( radians( degrees))
     
-    side = which_side(degrees)
+    side = _which_side(degrees)
     points = {  RIGHT:  [rad, y],
                 TOP:    [x, rad],
                 LEFT:   [-rad, y],
@@ -176,7 +176,7 @@ def side_point_for_degrees( degrees, rad):
     
     return points[side]
 
-def which_side( degrees):
+def _which_side( degrees):
     # sides:  0: right, 1: top, 2:left, 3:bottom
     if degrees > 315 or degrees <=45:   return RIGHT
     if degrees > 45 and degrees <=135:  return TOP
@@ -238,4 +238,131 @@ def bounding_box( points):
     return [ [min(all_x), min(all_y), min(all_z)], [max(all_x), max(all_y), max(all_z)]]
     
 
+
+# =======================
+# = Hardware dimensions =
+# =======================
+screw_dimensions = {
+    'm3': { 'nut_thickness':2.4, 'nut_inner_diam': 5.4, 'nut_outer_diam':6.1, 'screw_outer_diam':2.9},
+
+}
+
+# ==============
+# = Transforms =
+# ==============
+def transform_to_point( body, point, normal, two_d=False):
+    '''
+    Transforms body from horizontal at the origin to point, rotating it so
+    vertical now matches the supplied normal.
     
+    If two_d is False, rotate the up vector ( [0,0,1]) to match normal.
+    If two_d is True, assume we're functioning in XY, and rotate [0,1] to match normal
+    
+    This is useful for adding objects to arbitrary points on existing objects
+    
+    Returns body, transformed appropriately
+    
+    Use case:
+        -- make a 2-d shape that will be the side of an acrylic box
+        -- identify points on that shape that will need t-slots, and the normals
+            to the sides where the slots will be added
+        -- draw the t-slot shape at the origin, facing up.
+        -- at each point you want to place a slot, call 
+            transform_to_point( t_slot_poly, p, n, two_d=True)
+    '''
+    # TODO: move euclid  functions to a separate file
+    from pyeuclid.euclid import Vector3
+    
+    if two_d:
+        up = FORWARD_VEC
+    else:
+        up = UP_VEC
+    
+    euc_up = Vector3( *up)
+    euc_norm = Vector3( *normal)
+    
+    rot_angle = degrees( euc_norm.angle_between( euc_up))
+    rot_vec = euc_up.cross( euc_norm).as_arr()
+    
+    if rot_angle == 180:
+        rot_vec = up
+            
+    # # ETJ DEBUG
+    # print "************************************************************"
+    # classOrFile = self.__class__.__name__ if 'self' in vars() else os.path.basename(__file__)
+    # method = sys._getframe().f_code.co_name
+    # print "%(classOrFile)s:%(method)s"%vars()
+    # print '\trot_angle:  %s'% rot_angle
+    # print '\trot_vec:    %s'% rot_vec
+    # print "************************************************************"
+    # 
+    # # END DEBUG
+    
+    
+    
+    # TODO: figure out how to get these points
+    return translate( point)(
+                rotate( a=rot_angle, v=rot_vec)(
+                    body
+                )
+            )
+
+
+## {{{ http://code.activestate.com/recipes/577068/ (r1)
+def frange(*args):
+    """frange([start, ] end [, step [, mode]]) -> generator
+    
+    A float range generator. If not specified, the default start is 0.0
+    and the default step is 1.0.
+    
+    Optional argument mode sets whether frange outputs an open or closed
+    interval. mode must be an int. Bit zero of mode controls whether start is
+    included (on) or excluded (off); bit one does the same for end. Hence:
+        
+        0 -> open interval (start and end both excluded)
+        1 -> half-open (start included, end excluded)
+        2 -> half open (start excluded, end included)
+        3 -> closed (start and end both included)
+    
+    By default, mode=1 and only start is included in the output.
+    """
+    mode = 1  # Default mode is half-open.
+    n = len(args)
+    if n == 1:
+        args = (0.0, args[0], 1.0)
+    elif n == 2:
+        args = args + (1.0,)
+    elif n == 4:
+        mode = args[3]
+        args = args[0:3]
+    elif n != 3:
+        raise TypeError('frange expects 1-4 arguments, got %d' % n)
+    assert len(args) == 3
+    try:
+        start, end, step = [a + 0.0 for a in args]
+    except TypeError:
+        raise TypeError('arguments must be numbers')
+    if step == 0.0:
+        raise ValueError('step must not be zero')
+    if not isinstance(mode, int):
+        raise TypeError('mode must be an int')
+    if mode & 1:
+        i, x = 0, start
+    else:
+        i, x = 1, start+step
+    if step > 0:
+        if mode & 2:
+            from operator import le as comp
+        else:
+            from operator import lt as comp
+    else:
+        if mode & 2:
+            from operator import ge as comp
+        else:
+            from operator import gt as comp
+    while comp(x, end):
+        yield x
+        i += 1
+        x = start + i*step
+
+## end of http://code.activestate.com/recipes/577068/ }}}
