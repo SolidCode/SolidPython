92e662e09219495b3a59d3e996f40a1357d5aa00
Evan Jones <evan_t_jones@mac.com>
2013-02-13 12:41:21 +0100

    Added required metadata for PyPI submission

--- setup.py
+++ setup.py
@@ -3,8 +3,26 @@ from setuptools import setup, find_packages
 
 setup(name='solidpython',
       version='0.1',
+      description='Python interface to the OpenSCAD declarative geometry language',
+      author='Evan Jones',
+      author_email='evan_t_jones@mac.com',
+      url='https://github.com/SolidCode/SolidPython',
+      py_modules=['solid'],
+      classifiers = [
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
+          "Development Status :: 4 - Beta",
+          "Environment :: Other Environment",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
+          "Operating System :: OS Independent",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "Topic :: Scientific/Engineering :: Mathematics",
+          ],
       packages = find_packages(),  
       data_files = [ ('solid/test',['solid/test/run_all_tests.sh'])],   
-      install_requires = ['euclid'] ,
+      install_requires = ['euclid'] ,          
       )
+
+
 
