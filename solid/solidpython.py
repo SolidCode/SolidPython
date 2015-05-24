da580260154a40489c0aa361706f2edd245b644c
Nicholas Bollweg <nick.bollweg@gmail.com>
2015-05-24 00:52:00 -0400

    adding _repr_png_ for ipython

--- solid/solidpython.py
+++ solid/solidpython.py
@@ -602,6 +602,34 @@ class OpenSCADObject(object):
         '''
         return intersection()(self, x)
 
+    def _repr_png_(self):
+        '''
+        Allow rich clients such as the IPython Notebook, to display the current
+        OpenSCAD rendering of this object.
+        '''
+        png_data = None
+        tmp = tempfile.NamedTemporaryFile(suffix=".scad", delete=False)
+        tmp_png = tempfile.NamedTemporaryFile(suffix=".png", delete=False)
+        try:
+            scad_text = scad_render(self).encode("utf-8")
+            tmp.write(scad_text)
+            tmp.close()
+            tmp_png.close()
+            subprocess.Popen([
+                "openscad",
+                "--preview",
+                "-o", tmp_png.name,
+                tmp.name
+            ]).communicate()
+
+            with open(tmp_png.name, "rb") as png:
+                png_data = png.read()
+        finally:
+            os.unlink(tmp.name)
+            os.unlink(tmp_png.name)
+
+        return png_data
+
 
 class IncludedOpenSCADObject(OpenSCADObject):
 
