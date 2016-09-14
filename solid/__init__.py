e353f7dcebba5ebce0d885a752144aba973fd903
Eric Wieser <wieser.eric@gmail.com>
2016-09-14 16:27:03 -0400

    Fix bad imports that prefer a global file called 'solidpython'

--- solid/__init__.py
+++ solid/__init__.py
@@ -1,7 +1,4 @@
 # Some __init__ magic so we can include all solidpython code with:
 #   from solid import *
 #   from solid.utils import *
-try:
-    from solidpython import *
-except ImportError:
-    from solid.solidpython import *
+from .solidpython import *
