53810cf183fb15ce548aea859a65f9e8093e3cb9
Eric Wieser <wieser.eric@gmail.com>
2017-01-02 17:36:18 +0000

    Move all the basic openscad objects into a new file
    This almost halves the size of the main file.
    Note that a circular import is necessary, since the base class references the
    subclasses

--- solid/__init__.py
+++ solid/__init__.py
@@ -1,7 +1,5 @@
 # Some __init__ magic so we can include all solidpython code with:
 #   from solid import *
 #   from solid.utils import *
-try:
-    from solidpython import *
-except ImportError:
-    from solid.solidpython import *
+from .solidpython import *
+from .objects import *
