fb6b14c6b6f6feb54406519bc66640a83130c944
Evan Jones <evan_t_jones@mac.com>
2017-12-29 12:39:30 -0500

    Update objects.py
    moved 'convexity' arg to the end of rotate_extrude, since it's least-often used

--- solid/objects.py
+++ solid/objects.py
@@ -403,18 +403,18 @@ class rotate_extrude(OpenSCADObject):
 
     :param angle: Defaults to 360. Specifies the number of degrees to sweep, starting at the positive X axis. The direction of the sweep follows the Right Hand Rule, hence a negative angle will sweep clockwise.
     :type angle: number
+    
+    :param segments: The fixed number of fragments to use.
+    :type segments: int
 
     :param convexity: The convexity parameter specifies the maximum number of front sides (back sides) a ray intersecting the object might penetrate. This parameter is only needed for correctly displaying the object in OpenCSG preview mode and has no effect on the polyhedron rendering.
     :type convexity: int
 
-    :param segments: The fixed number of fragments to use.
-    :type segments: int
-
     '''
     def __init__(self, angle=360, convexity=None, segments=None):
         OpenSCADObject.__init__(self, 'rotate_extrude',
-                                {'angle': angle, 'convexity': convexity,
-                                 'segments': segments})
+                                {'angle': angle, 'segments': segments, 
+                                 'convexity': convexity})
 
 
 class dxf_linear_extrude(OpenSCADObject):
