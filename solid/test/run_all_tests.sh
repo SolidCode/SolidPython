1171f9aae02a57717cadcf3b08a527694ccb9bec
Evan Jones <evan_jones@mac.com>
2018-09-25 14:10:23 -0500

    remove the need for `run_all_tests.sh` to be run from `solid/test`

--- solid/test/run_all_tests.sh
+++ solid/test/run_all_tests.sh
@@ -1,7 +1,15 @@
-# Note that this file needs to be run from solid/test.  
+#!/usr/bin/env bash
+
+# Set CWD to this script's directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd $DIR
+
 for i in test_*.py;
 do 
 echo $i;
 python $i;
 echo
 done
+
+# revert to original dir
+cd -
