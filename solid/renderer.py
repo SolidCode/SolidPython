6e045b9e1f06cab570169acb59cdd5af26cc56bd
Nick Choly <nickcholy@gmail.com>
2018-11-12 11:11:24 -0500

    allow direct writing of stl files; automatically assume high

--- solid/renderer.py
+++ solid/renderer.py
@@ -5,7 +5,10 @@ import platform
 from IPython.display import display
 from ipywidgets import HTML, Text, Output, VBox
 from traitlets import link, dlink
-import math as pymath, os, time, subprocess
+import math as pymath
+import os
+import time
+import subprocess
 from pythreejs import *
 from IPython.display import display, SVG
 
@@ -13,22 +16,24 @@ from IPython.display import display, SVG
 OBJ_COLOR = '#f7d62c'
 BACKGROUND_COLOR = '#ffffff'
 
+
 class JupyterRenderer:
-    '''This class will render an OpenSCAD object within a jupyter notebook.  
-    pythreejs must be installed (see directions at
+    '''This class will render an OpenSCAD object within a jupyter notebook.
+    pythreejs must be installed (see directions a
     https://github.com/jupyter-widgets/pythreejs).
     This will try to guess the location of the openscad command-line executable,
     but you may optionally specify the path of the executable with the
     openscad_executable keyword.
 
-    This class needs to know the path to the openscad command-line tool, and a path of a temp directory.  
+    This class needs to know the path to the openscad command-line tool, and a path of a temp directory.
     You can set these locations with the OPENSCAD_EXEC and OPENSCAD_TMP_DIR environment variables,
     or you can set the "openscad_exec" and openscad_mpDir keyword options in the constructor.
     '''
     def __init__(self, **kw):
         self.openscad_exec = None
+        self.openscad_tmp_dir = None
         if 'OPENSCAD_EXEC' in os.environ: self.openscad_exec = os.environ['OPENSCAD_EXEC']
-        if 'OPENSCAD_TMP_DIR' in os.environ: self.openscadTmpDir = os.environ['OPENSCAD_TMP_DIR']
+        if 'OPENSCAD_TMP_DIR' in os.environ: self.openscad_tmp_dir = os.environ['OPENSCAD_TMP_DIR']
         if 'openscad_exec' in kw: self.openscad_exec = kw['openscad_exec']
         if self.openscad_exec is None:
             self._try_detect_openscad_exec()
@@ -38,12 +43,10 @@ class JupyterRenderer:
         self.height = kw.get('height', 600)
         self.draw_grids = kw.get('draw_grids', True)
 
-
     def _try_executable(self, executable_path):
         if os.path.isfile(executable_path):
             self.openscad_exec = executable_path
-        
-        
+
     def _try_detect_openscad_exec(self):
         self.openscad_exec = None
         platfm = platform.system()
@@ -54,11 +57,10 @@ class JupyterRenderer:
         elif platfm == 'Darwin':
             self._try_executable('/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD')
         elif platfm == 'Windows':
-            self._try_executable(os.path.join( \
-                    os.environ.get('Programfiles(x86)','C:'), \
+            self._try_executable(os.path.join(
+                    os.environ.get('Programfiles(x86)','C:'),
                     'OpenSCAD\\openscad.exe'))
 
-            
     def _conv_stl(self, stl_file_name):
         fl = open(stl_file_name)
         faces = []
@@ -83,7 +85,6 @@ class JupyterRenderer:
 
         return vertices, faces
 
-    
     def _get_extents(self, vertices):
         extents = []
         for i in range(3):
@@ -91,7 +92,6 @@ class JupyterRenderer:
             extents.append([min(coords), max(coords)])
         return extents
 
-    
     def _get_grid_lines(self, axis1, start, step, N, axis2, start2, end2):
         w = start
         vertices = []
@@ -106,7 +106,6 @@ class JupyterRenderer:
             vertices.append(pt2)
         return vertices
 
-    
     def _get_grids(self, obj_vertices):
         extents = self._get_extents(obj_vertices)
         grid_verts = []
@@ -121,59 +120,75 @@ class JupyterRenderer:
                 axis3 = [x for x in [0,1,2] if x not in [axis1, axis2]][0]
                 if axis1 == axis2: continue
                 delta = extents[axis1][1] - extents[axis1][0]
-                start = pymath.floor(extents[axis1][0] / space) * space        
-                start2 = pymath.floor(extents[axis2][0] / space) * space 
+                start = pymath.floor(extents[axis1][0] / space) * space
+                start2 = pymath.floor(extents[axis2][0] / space) * space
                 end2 = start2 + (N - 1) * space
                 verts = self._get_grid_lines(axis1, start, space, N, axis2,
                                              start2, end2)
                 grid_verts.extend(verts)
                 grid_cols.extend([axis_cols[axis3] for vert in verts])
 
-            
+
         lines_geom = Geometry(vertices=grid_verts, colors =grid_cols)
-        lines = LineSegments(geometry=lines_geom, 
-                 material=LineBasicMaterial(linewidth=5, transparent=True, 
-                 opacity=0.5, dashSize=10, 
-                 gapSize=10, vertexColors='VertexColors'), 
+        lines = LineSegments(geometry=lines_geom,
+                 material=LineBasicMaterial(linewidth=5, transparent=True,
+                 opacity=0.5, dashSize=10,
+                 gapSize=10, vertexColors='VertexColors'),
                  type='LinePieces')
-        
+
         return lines, space
 
-    
-    def render(self, py_scad_obj, **kw):
-        tmp_dir = tempfile.mkdtemp()
-        saved_umask = os.umask(0o077)        
-        scad_tmp_file = os.path.join(tmp_dir, 'tmp.scad')
-        stl_tmp_file = os.path.join(tmp_dir, 'tmp.stl')
-        scad_prepend = ''
+    def render_to_stl_file(self, py_scad_obj, fl_name, **kw):
+        scad_prepend = ''        
         if 'dollar_sign_vars' in kw:
             for var_name, value in kw['dollar_sign_vars'].items():
                 scad_prepend += '${}={};\n'.format(var_name, value)
+        else:
+            if not kw.get('rough', False):
+                scad_prepend += '$fn=120;\n'
+
+        scad_tmp_file = os.path.join(self.tmp_dir, 'tmp.scad')
         try:
             of = open(scad_tmp_file, 'w')
             of.write(scad_prepend)
             of.write(solid.scad_render(py_scad_obj))
             of.close()
-            
+
             # now run openscad to generate stl:
-            cmd = [self.openscad_exec, '-o', stl_tmp_file, scad_tmp_file]
+            cmd = [self.openscad_exec, '-o', fl_name, scad_tmp_file]
             return_code = subprocess.call(cmd)
             if return_code < 0:
                 raise Exception('openscad command line returned code {}'.format(return_code))
-            
-            self._render_stl(stl_tmp_file)
         except Exception as e:
             raise e
         finally:
             if os.path.isfile(scad_tmp_file):
-                os.remove(scad_tmp_file)
+                os.remove(scad_tmp_file)        
+
+    def render(self, py_scad_obj, **kw):
+        if self.openscad_tmp_dir is not None:
+            self.tmp_dir = self.openscad_tmp_dir
+        else:
+            self.tmp_dir = tempfile.mkdtemp()
+        self.saved_umask = os.umask(0o077)
+
+        stl_tmp_file = os.path.join(self.tmp_dir, 'tmp.stl')
+        
+        try:
+            kw['rough'] = True
+            self.render_to_stl_file(py_scad_obj, stl_tmp_file, **kw)
+            self._render_stl(stl_tmp_file)
+        except Exception as e:
+            raise e
+        finally:
             if os.path.isfile(stl_tmp_file):
                 os.remove(stl_tmp_file)
-            os.rmdir(tmp_dir)
-        
+            if self.openscad_tmp_dir is not None:
+                os.rmdir(self.tmp_dir)
+
     def _render_stl(self, stl_file):
         vertices, faces = self._conv_stl(stl_file)
-        
+
         # Map the vertex colors into the 'color' slot of the faces
         faces = [f + [None, [OBJ_COLOR for i in f], None] for f in faces]
 
@@ -188,8 +203,8 @@ class JupyterRenderer:
             geometry=obj_geometry,
             material=MeshLambertMaterial(vertexColors='VertexColors'),
             position=[0, 0, 0],   # Center the cube
-        )        
-        
+        )
+
         n_vert = len(vertices)
         center = [sum([vertex[i] for vertex in vertices]) / float(n_vert)
                   for i in range(3)]
@@ -206,15 +221,15 @@ class JupyterRenderer:
 
         scene_things = [my_object_mesh, camera, AmbientLight(color='#888888')]
         if self.draw_grids:
-            grids, space = self._get_grids(vertices)            
+            grids, space = self._get_grids(vertices)
             scene_things.append(grids)
-        
+
         scene = Scene(children=scene_things, background=BACKGROUND_COLOR)
 
-        renderer_obj = Renderer(camera=camera, background='#cccc88', \
-            background_opacity=0, scene=scene, \
-            controls=[OrbitControls(controlling=camera)], \
-            width=self.width, \
+        renderer_obj = Renderer(camera=camera, background='#cccc88',
+            background_opacity=0, scene=scene,
+            controls=[OrbitControls(controlling=camera)],
+            width=self.width,
             height=self.height)
 
         display_things = [renderer_obj]
@@ -226,6 +241,6 @@ class JupyterRenderer:
   Sorry, your browser does not support inline SVG.
 </svg>""".format(self.width, self.width//2, self.width//2+25, space)
             display_things.append(SVG(data=s))
-        
+
         display(*display_things)
-        
+
