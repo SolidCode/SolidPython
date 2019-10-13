3d4b8804324bfd13464ea436a883b344b8a736dd
Ryan Jarvis <ryan@technoloft.com>
2019-10-13 13:16:05 -0700

    Small fixes to shell script

--- solid/examples/run_all_examples.sh
+++ solid/examples/run_all_examples.sh
@@ -2,28 +2,27 @@
 
 # Set CWD to this script's directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-cd $DIR
+cd "$DIR" || exit 1
 
 COMPILED_EXAMPLES=${PWD}/Compiled_examples
 
 echo
 # if COMPILED_EXAMPLES doesn't exist, create it.
-if [ ! -e $COMPILED_EXAMPLES ]; 
-     then mkdir $COMPILED_EXAMPLES;     
+if [ ! -e "$COMPILED_EXAMPLES" ];
+     then mkdir "$COMPILED_EXAMPLES";
 fi    
  
  function run_example {
     echo "===================================================";
-    # echo "python $1 $2";
-    python $1 $2;  
+    python "$1 $2";
     echo "===================================================";
 }
 
 for py in *.py;
-do run_example $py $COMPILED_EXAMPLES;
+do run_example "$py $COMPILED_EXAMPLES";
 done 
 
-run_example mazebox/mazebox.py $COMPILED_EXAMPLES;
+run_example mazebox/mazebox.py "$COMPILED_EXAMPLES";
 
 # revert to original dir
-cd -
+cd - || exit 1
