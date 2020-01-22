bf9c55dc496cdc93284cf27ec2229f1261c197da
Evan Jones <evan_jones@mac.com>
2020-01-22 16:49:16 -0600

    Added `implicitcad_render_to_file()` Non-consumer methods in implicit.py given _leading _underscores to show they're not useful. Added `suffix` argument to `solid.scad_render_to_file()` and `solid._write_code_to_file()` so we can output to `X.escad` as needed

--- solid/implicit.py
+++ solid/implicit.py
@@ -1,14 +1,15 @@
 #! /usr/bin/env python
 import solid.objects
-from solid.solidpython import calling_module, OpenSCADObject
+from solid.solidpython import calling_module, OpenSCADObject, scad_render_to_file, PathStr
 
 from textwrap import dedent
 from typing import  Dict, Callable
 
-def implicitCAD_patch_objects():
+def _implicitCAD_patch_objects():
     """
     Usage: 
     `from solid.implicit import *`, (rather than `from solid import *`)
+    from solid.implicit import cube, union, square, 
     All other usage should be like normal SolidPython.
 
     Monkey-patching for basic compatibility with Christopher Olah's ImplicitCAD
@@ -21,15 +22,15 @@ def implicitCAD_patch_objects():
     """
     to_add_rad = [ 
         'square', 'polygon', 
-        'cube', 'cylinder', 'polyhedron', 
+        'cube',  'polyhedron', 
         'union', 'intersection', 'difference'
-    ]
+    ] # 'cylinder' excluded; it already has an 'r' arg
         
     cur_mod = calling_module(stack_depth=1)
 
     for cls_name, cls in solid.objects.__dict__.items():
         if cls_name in to_add_rad:
-            args_dict_orig = method_args_dict(cls.__init__)
+            args_dict_orig = _method_args_dict(cls.__init__)
             args_str_orig = ', '.join(['self'] + [f'{k} = {v}' for k, v in args_dict_orig.items()])
             args_str = ', '.join([args_str_orig, 'r = 0'])
             super_args_str = ', '.join([f'{k} = {k}' for k in args_dict_orig.keys()])
@@ -48,11 +49,21 @@ def implicitCAD_patch_objects():
         # Add the class (original or new subclass) to this module's top level namespace
         setattr(cur_mod, cls_name, cls)
 
-def method_args_dict(method:Callable) -> Dict:
+def _method_args_dict(method:Callable) -> Dict:
     var_names = method.__code__.co_varnames
     var_names = tuple((v for v in var_names if v is not 'self'))
     default_vals = method.__defaults__ or [None] # type:ignore
 
     return dict(zip(var_names, default_vals))
 
-implicitCAD_patch_objects()
+def implicitcad_render_to_file(scad_object: OpenSCADObject,
+                        filepath: PathStr=None, 
+                        out_dir: PathStr=None,
+                        file_header: str='', 
+                        include_orig_code: bool=True) -> str:
+    return scad_render_to_file(scad_object, filepath, out_dir, file_header, 
+                                include_orig_code, suffix='.escad')
+            
+
+
+_implicitCAD_patch_objects()
