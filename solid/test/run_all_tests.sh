1ece137e5802b4486405e6d3975067e53758fff9
jeff <1105041+jeff-dh@users.noreply.github.com>
2021-05-19 18:57:15 +0200

    (hopefully) made run_all_tests.sh more portable 	- I hope this works for everybody else, but I think it should

--- solid/test/run_all_tests.sh
+++ solid/test/run_all_tests.sh
@@ -4,12 +4,13 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd $DIR
 
+export PYTHONPATH="../../":$PYTHONPATH
 # Run all tests. Note that unittest's built-in discovery doesn't run the dynamic 
 # testcase generation they contain
 for i in test_*.py;
 do 
     echo $i;
-    python $i;
+    python3 $i;
     echo
 done
 
